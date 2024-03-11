2fbc52bb7770783f98eceeccd2bd3bc6608a3fcd
Irina Saveleva <goyalion17@gmail.com>
2024-03-11 18:49:29 +0100

    server component vs client component

--- app/client/page.js
+++ app/client/page.js
@@ -1,9 +1,18 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const ClientPage = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div>
-      <h1 className="text-7xl">Client Page</h1>
+      <h1 className="text-7xl font-bold mb-4">{counter}</h1>
+      <button
+        onClick={() => setCounter((cur) => cur + 1)}
+        className="btn btn-primary"
+      >
+        increase
+      </button>
     </div>
   );
 };
