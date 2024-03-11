3f36c0054c8c1bbb13631d498225a025e3889657
Irina Saveleva <goyalion17@gmail.com>
2024-03-11 18:58:40 +0100

    fetch data

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,6 +1,12 @@
 import React from "react";
 
-const DrinksPage = () => {
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+
+const DrinksPage = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+
   return (
     <div>
       <h1 className="text-7xl">Drinks Page</h1>
