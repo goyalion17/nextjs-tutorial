f87d6568115c0e43c900fb9ca54f66953352574a
Irina Saveleva <goyalion17@gmail.com>
2024-03-12 15:53:34 +0100

    static and remote images

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
@@ -13,14 +14,21 @@ const SingleDrinkPage = async ({ params }) => {
   const data = await getSingleDrink(params.id);
   const title = data?.drinks[0]?.strDrink;
   const imgSrc = data?.drinks[0]?.strDrinkThumb;
-  console.log(title, imgSrc);
+
   return (
     <div>
       <Link href="/drinks" className="btn btn-primary mt-8 mb-12">
         back to drinks
       </Link>
+      <Image
+        src={imgSrc}
+        className="w-48 h-48 rounded shadow-lg mb-4"
+        width={300}
+        height={300}
+        priority
+        alt={title}
+      />
       <h1 className="text-4xl mb-8">{title}</h1>
-      <img src={imgSrc} />
     </div>
   );
 };
