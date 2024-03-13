35b7504a7c03f9491da3f3a76c69d0a3898a5a8d
Irina Saveleva <goyalion17@gmail.com>
2024-03-13 20:36:15 +0100

    refactoring app

---
+++ utils/actions.js
@@ -0,0 +1,20 @@
+"use server";
+import prisma from "../utils/db";
+
+export const getAllTasks = async () => {
+  return await prisma.task.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+};
+
+export const createTask = async (formData) => {
+  const content = formData.get("content");
+  await prisma.task.create({
+    data: {
+      content,
+    },
+  });
+  revalidatePath("/tasks");
+};
