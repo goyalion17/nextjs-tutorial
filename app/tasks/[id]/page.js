ae7cbfd9fdaab501da66b9e86f6b361aba1d81ad
Irina Saveleva <goyalion17@gmail.com>
2024-03-14 12:52:25 +0100

    editing task - complete

--- app/tasks/[id]/page.js
+++ app/tasks/[id]/page.js
@@ -7,12 +7,14 @@ const SingleTaskPage = async ({ params }) => {
   const task = await getTask(params.id);
 
   return (
-    <div className="mb-16">
-      <Link href="/tasks" className="btn btn-accent">
-        back to tasks
-      </Link>
+    <>
+      <div className="mb-16">
+        <Link href="/tasks" className="btn btn-accent">
+          back to tasks
+        </Link>
+      </div>
       <EditForm task={task} />
-    </div>
+    </>
   );
 };
 
